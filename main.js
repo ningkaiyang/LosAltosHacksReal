49caea2a1f31262ca471848ff9186f18ae90f765
ningkaiyang <ningkaiyang@gmail.com>
2023-04-09 03:55:01 -0700

    my god
    Co-authored-by: gracexu24 <gracexu24@users.noreply.github.com>
    Co-authored-by: BPJENNIEROSE <BPJENNIEROSE@users.noreply.github.com>
    Co-authored-by: ngawang <toiledejouy@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var numtask = prompt("How many tasks do you have today?");
 var tasks = new Array();
+var completecount = 1; 
 
 for(var i = 0; i<numtask; i++){
     tasks.push(prompt("Enter Task")); 
@@ -17,56 +18,110 @@ for (var i = 0; i <tasks.length; i++) {
 var doneButton = document.getElementById("task-button");
 doneButton.addEventListener("click", complete);
 
-function complete() {
-    var tasknum = prompt("Which task number did you complete?");
-    var taskelem = document.getElementById("list-item-" + tasknum);
-    taskelem.style.backgroundImage = "url('assets/CheckedBox.png')";
+function buildhouse(){
     var rannum = Math.floor((Math.random()*25) + 1);    // 1-25
     while (document.getElementById("thing-" + rannum).getAttribute("src") != "assets/unnamed.png" ){
         rannum = Math.floor((Math.random()*25) + 1);  
     }
-    var ranhouse = Math.floor((Math.random()*3) + 1); 
+    var ranhouse = Math.floor((Math.random()*3) + 1);
     if (ranhouse === 1){
-        for (var i = 1; i<28; i++){
-            var image = document.getElementById("thing-" + rannum);
-            var name = ("assets/first-block-" + i + ".png.png");   // more debugging
-            setTimeout(function(){image.setAttribute("src", name)}, 9000); 
+        var incrementer = 1;
+        intervalId = setInterval(displayNext, 200);
+
+        function stop(){
+            clearInterval(intervalId);
         }
+
+        function displayNext(){
+            var image = document.getElementById("thing-" + rannum);
+            var name = ("assets/first-block-" + incrementer + ".png.png");
+            image.setAttribute("src", name);
+            if (incrementer >= 27){
+                stop();
+            }
+            incrementer++;
+        }
+        completecount++;
     }
-    if (ranhouse === 2){
-        for (var i = 28; i<39; i++){
-            var image = document.getElementById("thing-" + rannum);
-            var name = ("assets/first-block-" + i + ".png.png");   // more debugging
-            setTimeout(function(){image.setAttribute("src", name)}, 9000); 
+    if (ranhouse === 2){ 
+        var incrementer = 28;
+        intervalId = setInterval(displayNext, 200);
+
+        function stop(){
+            clearInterval(intervalId);
         }
+
+        function displayNext(){
+            var image = document.getElementById("thing-" + rannum);
+            var name = ("assets/first-block-" + incrementer + ".png.png");
+            image.setAttribute("src", name);
+            if (incrementer >= 38){
+                stop();
+            }
+            incrementer++;
+        }
+        completecount++;
     }
     if (ranhouse === 3){
-        for (var i = 39; i<50; i++){
-            var image = document.getElementById("thing-" + rannum);
-            var name = ("assets/first-block-" + i + ".png.png");   // more debugging
-            setTimeout(function(){image.setAttribute("src", name)}, 9000); 
-        }
-    }
+        var incrementer = 39;
+        intervalId = setInterval(displayNext, 200);
 
-    if(check()){
-        win();
+        function stop(){
+            clearInterval(intervalId);
+        }
+
+        function displayNext(){
+            var image = document.getElementById("thing-" + rannum);
+            var name = ("assets/first-block-" + incrementer + ".png.png");
+            image.setAttribute("src", name);
+            if (incrementer >= 49){
+                stop();
+            }
+            incrementer++;
+        }
+        completecount++;
     }
+}
+
+function complete() {
+    var tasknum = prompt("Which task number did you complete?");
+    var taskelem = document.getElementById("list-item-" + tasknum);
+    taskelem.style.backgroundImage = "url('assets/CheckedBox.png')";
+    console.log(check());
+    if(check()){
+        console.log("Timeout TEST?!?!?!!?!?");
+        setTimeout(win(), 5000);
+    }
+    buildhouse();
 }
 
 function check(){
-    // Loop through all tasks number, and check to see if they are equal to CheckedBox.
-    // If all === CheckedBox then run win condition function
-    for (var i = 0; i <tasks.length; i++) {
-        var item = document.getElementById("list-item-" + (i+1)); 
-        var value2Check = getComputedStyle(item);
-        var backgroundValue = value2Check.backgroundImage;
-        if(backgroundValue === "url('assets/UncheckedBox.png')"){
-            return false;
-        }
+    console.log(completecount);
+    if(completecount === tasks.length){
+        return true;
     }
-    return true;
+    else{
+        return false;
+    }
 }
 
 function win(){
-    // Turn all houses to their respective value shiny sprite (1, 2, 3) house type 
+    // Turn all houses to their respective value shiny sprite (1, 2, 3) house type
+    for (var i = 1; i<26; i++){
+        console.log(i);
+        var img = document.getElementById("thing-" + i);
+        var src = img.getAttribute("src");
+        if (src === "assets/first-block-27.png.png"){
+            console.log('Funny monkey');
+            img.setAttribute("src", "assets/ezgif-3-887a01b628.gif")
+        }
+        else if (src === "assets/first-block-38.png.png"){
+            console.log('Funny monkey');
+            img.setAttribute("src", "assets/ezgif-3-32116975a.gif")
+        }
+        else if (src === "assets/first-block-49.png.png"){
+            console.log('Funny monkey');
+            img.setAttribute("src", "assets/ezgif-3-fdbfaea6c1.gif")
+        }
+    } 
 }
